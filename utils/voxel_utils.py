a73f550b6ae0ad4c47570dc186f3c98c2a8fd8e9
Bjarke <bjarke.hastrup@gmail.com>
2020-10-27 23:55:13 +0100

    Bug fix in voxel

--- utils/voxel_utils.py
+++ utils/voxel_utils.py
@@ -1,4 +1,4 @@
-
+import time
 import numpy as np
 import torch
 
@@ -24,7 +24,7 @@ def get_3d_grid(unit_cell_len, n_grid_points):
     return grid_3d
 
 
-def get_voxel_repr(atom_object, agent_atom, pos_goal, grid_3d, n_grid, radius=3,
+def get_voxel_repr(atom_object, agent_atom, pos_goal, grid_3d, n_grid, box_length, radius=3,
                    sigma=0.3, device=torch.device("cpu")):
     """
         Calculates the total voxel density from an array of Cartesian coordinates
@@ -32,16 +32,17 @@ def get_voxel_repr(atom_object, agent_atom, pos_goal, grid_3d, n_grid, radius=3,
 
     # All atom positions
     pos = atom_object.get_positions()
-
+    
     # Position of agent atom
     pos_agent = pos[agent_atom]
-    
+
     # Unit cell
     cell = atom_object.get_cell()
     
     # Create enhanced cell using unit cell periodicity
     #   (speed up: maybe do if statemant to only enhance in
     #   some directions depending on positions of agent atom)
+    # start_time = time.time()
     pos_list = pos.tolist()
     pos_e = pos_list.copy()
     pos_e += (pos_list+cell[0]).tolist()
@@ -53,47 +54,65 @@ def get_voxel_repr(atom_object, agent_atom, pos_goal, grid_3d, n_grid, radius=3,
     pos_e += (pos_list-cell[0]+cell[1]).tolist()
     pos_e += (pos_list-cell[0]-cell[1]).tolist()
     pos_e = np.array(pos_e)
-    
-    # Remove agent atom from channel 1
-    pos_e = np.delete(pos_e, agent_atom, axis=0)
+    # print("ENHANCEMENT: --- %s seconds ---" % (time.time() - start_time))
     
     # assert radius > np.linalg.norm(env.predict_goal_location()-pos_agent)
+    # Remove agent atom from channel 1
+    pos_e = np.delete(pos_e, agent_atom, axis=0)
+
+    ##################################################################
+    ###### Create voxel density of surrounding atoms (channel1) ######
+    ##################################################################
     
     # Only keep atoms less than "radius" Ãƒâ€¦ from the agent atom
     pos_local = pos_e[np.linalg.norm(pos_e-pos_agent, axis=1) < radius]
 
     # Recenter the postions such that the agent is placed in the cube center
-    pos_conv = pos_local-pos_agent + np.repeat(np.sqrt(radius), 3)
-    
-    # Recenter the postions such that the agent is placed in the cube center
-    pos_conv_goal = pos_goal-pos_agent + np.repeat(np.sqrt(radius), 3)
+    pos_conv = pos_local-pos_agent + np.repeat(box_length*0.5, 3)
     
     # Number of nearby atoms
     n_atoms_local = pos_conv.shape[0]
-    
-    # Create voxel density of surrounding atoms (channel1)
+
+    # start_time = time.time()
     channel1 = np.zeros(n_grid ** 3)
     for i in range(n_atoms_local):
-        channel1 += np.exp(-(np.linalg.norm(grid_3d - pos[i,:],
+        channel1 += np.exp(-(np.linalg.norm(grid_3d - pos_conv[i,:],
                                     axis=1)**2) / (2*sigma**2))
-
+    # print("Channel1: --- %s seconds ---" % (time.time() - start_time))
     channel1 = np.reshape(channel1, (n_grid, n_grid, n_grid))
     
-    # Create voxel density of agent atom (channel2)
+    ##################################################################
+    ###### Create voxel density of agent atom (channel2) #############
+    ##################################################################
+    # start_time = time.time()
+
+    pos_agent_conv = pos_agent - pos_agent + np.repeat(box_length*0.5, 3)
+    # so silly, how should we actually represent the current agent state
+
     # (redundant if we always place it in the center anyway)
-    channel2 = np.exp(-(np.linalg.norm(grid_3d - pos_agent,
+    channel2 = np.exp(-(np.linalg.norm(grid_3d - pos_agent_conv,
                                     axis=1)**2) / (2*sigma**2))
 
     channel2 = np.reshape(channel2, (n_grid, n_grid, n_grid))
     
-    # Create voxel density of goal location (channel3)
-    channel3 = np.exp(-(np.linalg.norm(grid_3d - pos_goal,
+    ##################################################################
+    ###### Create voxel density of goal location (channel3) ##########
+    ##################################################################
+
+    # Recenter the postions
+    pos_conv_goal = pos_goal-pos_agent + np.repeat(box_length*0.5, 3)
+
+    channel3 = np.exp(-(np.linalg.norm(grid_3d - pos_conv_goal,
                                     axis=1)**2) / (2*sigma**2))
 
     channel3 = np.reshape(channel3, (n_grid, n_grid, n_grid))
+    # print("TWO MORE CHANNELS: --- %s seconds ---" % (time.time() - start_time))
+
+    ##################################################################
+    ###################### Stacking ##################################
+    ##################################################################
     
     voxel = np.stack((channel1, channel2, channel3))
-
     voxel = torch.from_numpy(voxel).double()
     
     return voxel.unsqueeze(0).to(device)
