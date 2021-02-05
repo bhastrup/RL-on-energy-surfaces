bbf2ad0ef0db10e9dfb51d0e2ed60a9ec0e855c6
bhastrup <bjarke.hastrup@gmail.com>
2021-02-05 01:39:19 +0100

    NEB optmization files

--- utils/slab_params.py
+++ utils/slab_params.py
@@ -2,39 +2,57 @@
 from ase import Atoms
 from ase.io import Trajectory
 from ase.io import write
-
-from ase.visualize import view # run view(atom_objects[0])
+from ase.visualize import view
 from ase.build import fcc111, add_adsorbate
-from ase.calculators.abinit import Abinit
-from ase.calculators.emt import EMT
-# from asap3 import EMT
-from ase.calculators.lj import LennardJones
+
+import imp
+try:
+    imp.find_module('asap3')
+    found_asap3 = True
+except ImportError:
+    found_asap3 = False
+
+if found_asap3:
+    from asap3 import EMT
+else:
+    from ase.calculators.emt import EMT
+
 from ase.optimize import BFGS
-from ase.constraints import FixAtoms
+from ase.constraints import FixAtoms, FixedPlane
 
 # Build ASE slab
 slab = fcc111('Cu', size=(2,3,4), vacuum=10.0)
 add_adsorbate(slab, 'Cu', 2.08, 'fcc')
 calc = EMT()
-#calc = LennardJones()
 slab.set_calculator(calc)
-#view(slab)
+
+# Specify agent atom
+n_atoms = len(slab)
+agent_atom = n_atoms - 1
+
+# Create constraint for bottom layer atoms in slabs
+#active_dist = 1.5
+#all_dists = slab.get_distances(
+#    a=agent_atom,
+#    indices=list(range(n_atoms)),
+#    mic=True
+#)
+#mask = all_dists > active_dist
+#constraint = FixAtoms(mask=mask)
+#slab.set_constraint(constraint)
+
 
 # Specify initial configuration, A
 dyn = BFGS(slab, trajectory='slab.traj')
-dyn.run(fmax=0.05)
+dyn.run(fmax=0.03)
 
 # Specify goal configuration, B
 slab_b = slab.copy()
 slab_b.set_calculator(calc)
 slab_b[-1].x += slab.get_cell()[0, 0] / 2
 dyn_B = BFGS(slab_b, trajectory='slab_B.traj')
-dyn_B.run(fmax=0.05)
-#view(slab_b)
+dyn_B.run(fmax=0.03)
 
-# Specify agent atom
-n_atoms = len(slab)
-agent_atom = n_atoms - 1
 
 # Calculated if agent atom is in goal hollow site
 hollow_neighbors = [12, 13, 14]
